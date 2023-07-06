31f1d188334a9ce2476c6c555ec9146886d950e0
Fahim Shariar <fahimshariar28@gmail.com>
2023-07-06 16:58:41 +0600

    Contact section added in home page

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
 import Banner from "./Banner/Banner";
+import Contact from "./Contact/Contact";
 import ProductSection from "./ProductSection/ProductSection";
 
 const HomePage = () => {
@@ -6,6 +7,7 @@ const HomePage = () => {
     <div>
       <Banner />
       <ProductSection />
+      <Contact />
     </div>
   );
 };
