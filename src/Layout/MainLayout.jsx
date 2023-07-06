b202b46b911a2e3a8cfadfc78ad6dc3c8589929c
Fahim Shariar <fahimshariar28@gmail.com>
2023-07-06 15:34:14 +0600

    Footer added and main layout set

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,10 +1,13 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <div>
-      <h1>Main Layout</h1>
       <Navbar />
+      <Outlet />
+      <Footer />
     </div>
   );
 };
